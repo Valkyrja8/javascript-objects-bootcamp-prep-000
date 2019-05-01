fd443cfa6e28a8fe6d3ea5300155d0c6187889f4
Anais Trope <trope.anais@gmail.com>
2019-05-01 11:50:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = { artistName: "songTitle"};
+<<<<<<< HEAD
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = 'songTitle';
@@ -9,3 +10,5 @@ function removeFromPlaylist(playlist, artistName){
  delete playlist[artistName]
  return playlist;
 }
+=======
+>>>>>>> 1d288a68ff2a07cf2bafdfc2bad7f3475ba924b8
